981da2851f00cb55f3d8748ec24398e6a5fee248
Sahil Raj <sahilraj7094@gmail.com>
2025-06-30 20:31:05 +0530

    Update  Largest element in array.cpp

--- Largest element in array.cpp
+++ Largest element in array.cpp
@@ -1,14 +1,14 @@
-Example 1:
-Input: arr[] = {2,5,1,3,0};
-Output: 5
-Explanation: 5 is the largest element in the array. 
+// Example 1:
+// Input: arr[] = {2,5,1,3,0};
+// Output: 5
+// Explanation: 5 is the largest element in the array. 
 
-Example2: 
-Input: arr[] = {8,10,5,7,9};
-Output: 10
-Explanation: 10 is the largest element in the array. 
+// Example2: 
+// Input: arr[] = {8,10,5,7,9};
+// Output: 10
+// Explanation: 10 is the largest element in the array. 
 
-Code :-Brute force Approach
+// Code :-Brute force Approach
 
 class solution{
 public:
