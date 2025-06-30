8acc77fb8302c77d14bd4d10987b172aee36db19
Sahil Raj <sahilraj7094@gmail.com>
2025-06-30 20:34:39 +0530

    Update Next Permutation.cpp

--- Next Permutation.cpp
+++ Next Permutation.cpp
@@ -1,13 +1,13 @@
-Example 1:
-Input: nums = [1,2,3]
-Output: [1,3,2]
-Example 2:
-Input: nums = [3,2,1]
-Output: [1,2,3]
-Example 3:
-Input: nums = [1,1,5]
-Output: [1,5,1]
-Code :-
+// Example 1:
+// Input: nums = [1,2,3]
+// Output: [1,3,2]
+// Example 2:
+// Input: nums = [3,2,1]
+// Output: [1,2,3]
+// Example 3:
+// Input: nums = [1,1,5]
+// Output: [1,5,1]
+// Code :-
 
 class Solution {
 public:
