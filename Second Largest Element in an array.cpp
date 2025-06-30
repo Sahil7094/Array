a2e6c675d51928f289398a373ad919ad8e996a1c
Sahil Raj <sahilraj7094@gmail.com>
2025-06-30 20:37:54 +0530

    Update Second Largest Element in an array.cpp

--- Second Largest Element in an array.cpp
+++ Second Largest Element in an array.cpp
@@ -1,17 +1,12 @@
-Examples:-
-Input: nums = [8, 8, 7, 6, 5]
+// Examples:-
+// Input: nums = [8, 8, 7, 6, 5]
+// Output: 7
+// Explanation: The largest value in nums is 8, the second largest is 7
+// Input: nums = [10, 10, 10, 10, 10]
+// Output: -1
+// Explanation: The only value in nums is 10, so there is no second largest value, thus -1 is returned
+// Code :-
 
-Output: 7
-
-Explanation: The largest value in nums is 8, the second largest is 7
-
-Input: nums = [10, 10, 10, 10, 10]
-
-Output: -1
-
-Explanation: The only value in nums is 10, so there is no second largest value, thus -1 is returned
-
-Code :-
 class Solution {
 public:
     int secondLargestElement(vector<int>& nums) {
