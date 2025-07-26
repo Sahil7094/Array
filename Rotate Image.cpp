32e8b6673fe7789cd8ce2381693cff68bd64ebdf
Sahil Raj <sahilraj7094@gmail.com>
2025-07-27 01:06:57 +0530

    Update Rotate Image.cpp

--- Rotate Image.cpp
+++ Rotate Image.cpp
@@ -10,6 +10,21 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        for(int i=0; i<n; i++){
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
+    }
+};
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
         int m=matrix.size();
         int temp[m][m];
         for(int i=0; i<m; i++){
