fa094d588f7f8e2b116b45565f1b78e9f9a7ac37
Sahil Raj <sahilraj7094@gmail.com>
2025-07-30 08:48:47 +0530

    Update Majority Element.cpp

--- Majority Element.cpp
+++ Majority Element.cpp
@@ -7,12 +7,72 @@
 // Input: nums = [2,2,1,1,1,2,2]
 // Output: 2
 // Code :- 
-
+//Brute force Solution :-
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        for(int i=0; i<n; i++){
+            int cnt=0;
+            for(int j=0; j<n; j++){
+                if(nums[i]==nums[j]) cnt++;
+            }
+            if(cnt>n/2) return nums[i];
+        }    
+        return {};
+    }
+};
+//Better solution:-
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        map<int,int>mpp;
+        for(int i=0; i<n; i++){
+            mpp[nums[i]]++;
+        }
+        for(auto it: mpp){
+            if(it.second>n/2){
+                return it.first;
+            }
+        }
+        return {};
+    }
+};
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int n = nums.size();
         return nums[n/2];
+    }
+};
+// Optimal Solution Moore's voting Algorithm :-
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int cnt=0;
+        int el;
+        for(int i=0; i<n; i++){
+            if(cnt==0){
+                cnt++;
+                el=nums[i];
+            }
+            else if(el==nums[i]){
+                cnt++;
+            }
+            else{
+                cnt--;
+            }
+        }
+        int cnt1=0;
+        for(int i=0; i<n; i++){
+            if(el==nums[i]){
+                cnt1++;
+            }
+        }
+        if(cnt1>n/2) return el;
+        return {};
     }
 };
